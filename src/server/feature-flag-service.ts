2e4b1ab403f59094f20ca4a585a53b2a90745de8
tau <mamehogetanuki@gmail.com>
2026-03-01 09:44:21 +0900

    feat: add feature flag service with env variable support

---
+++ src/server/feature-flag-service.ts
@@ -0,0 +1,44 @@
+import type { FeatureFlags, DEFAULT_FLAGS } from "../../shared/feature-flags";
+
+class FeatureFlagService {
+  private static instance: FeatureFlagService;
+
+  private flags: FeatureFlags;
+
+  private constructor() {
+    this.flags = this.loadFlags();
+  }
+
+  public static getInstance(): FeatureFlagService {
+    if (!FeatureFlagService.instance) {
+      FeatureFlagService.instance = new FeatureFlagService();
+    }
+    return FeatureFlagService.instance;
+  }
+
+  private loadFlags(): FeatureFlags {
+    return {
+      enableHeroSystem: this.parseEnvBoolean("FEATURE_ENABLE_HERO_SYSTEM"),
+      enableSharedPool: this.parseEnvBoolean("FEATURE_ENABLE_SHARED_POOL"),
+      enablePhaseExpansion: this.parseEnvBoolean("FEATURE_ENABLE_PHASE_EXPANSION"),
+    };
+  }
+
+  private parseEnvBoolean(envName: string): boolean {
+    const value = process.env[envName];
+    if (value === undefined) {
+      return false;
+    }
+    return value.toLowerCase() === "true" || value === "1";
+  }
+
+  public getFlags(): FeatureFlags {
+    return { ...this.flags };
+  }
+
+  public isFeatureEnabled(featureName: keyof FeatureFlags): boolean {
+    return this.flags[featureName] === true;
+  }
+}
+
+export { FeatureFlagService };

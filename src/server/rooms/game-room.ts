81197bf5c2ea26e7a8ca46343f507c77ce558d12
tau <mamehogetanuki@gmail.com>
2026-03-05 00:45:18 +0900

    refactor: extract buildPrepCommandPayload helper from handlePrepCommand

--- src/server/rooms/game-room.ts
+++ src/server/rooms/game-room.ts
@@ -277,72 +277,7 @@ export class GameRoom extends Room<{ state: MatchRoomState }> {
       message.correlationId,
     );
 
-    let commandPayload:
-      | {
-          boardUnitCount?: number;
-          boardPlacements?: NonNullable<PrepCommandMessage["boardPlacements"]>;
-          xpPurchaseCount?: number;
-          shopRefreshCount?: number;
-          shopBuySlotIndex?: number;
-          shopLock?: boolean;
-          benchToBoardCell?: {
-            benchIndex: number;
-            cell: number;
-          };
-          benchSellIndex?: number;
-          boardSellIndex?: number;
-        }
-      | undefined;
-
-    if (
-      message.boardUnitCount !== undefined ||
-      message.boardPlacements !== undefined ||
-      message.xpPurchaseCount !== undefined ||
-      message.shopRefreshCount !== undefined ||
-      message.shopBuySlotIndex !== undefined ||
-      message.shopLock !== undefined ||
-      message.benchToBoardCell !== undefined ||
-      message.benchSellIndex !== undefined ||
-      message.boardSellIndex !== undefined
-    ) {
-      commandPayload = {};
-
-      if (message.boardUnitCount !== undefined) {
-        commandPayload.boardUnitCount = message.boardUnitCount;
-      }
-
-      if (message.boardPlacements !== undefined) {
-        commandPayload.boardPlacements = message.boardPlacements;
-      }
-
-      if (message.xpPurchaseCount !== undefined) {
-        commandPayload.xpPurchaseCount = message.xpPurchaseCount;
-      }
-
-      if (message.shopRefreshCount !== undefined) {
-        commandPayload.shopRefreshCount = message.shopRefreshCount;
-      }
-
-      if (message.shopBuySlotIndex !== undefined) {
-        commandPayload.shopBuySlotIndex = message.shopBuySlotIndex;
-      }
-
-      if (message.shopLock !== undefined) {
-        commandPayload.shopLock = message.shopLock;
-      }
-
-      if (message.benchToBoardCell !== undefined) {
-        commandPayload.benchToBoardCell = message.benchToBoardCell;
-      }
-
-      if (message.benchSellIndex !== undefined) {
-        commandPayload.benchSellIndex = message.benchSellIndex;
-      }
-
-      if (message.boardSellIndex !== undefined) {
-        commandPayload.boardSellIndex = message.boardSellIndex;
-      }
-    }
+    const commandPayload = this.buildPrepCommandPayload(message);
 
     this.sharedBoardBridge?.logGameCommandEvent({
       playerId: client.sessionId,
@@ -558,6 +493,48 @@ export class GameRoom extends Room<{ state: MatchRoomState }> {
     }
 
     client.send(SERVER_MESSAGE_TYPES.COMMAND_RESULT, result);
+  }
+
+  private buildPrepCommandPayload(
+    message: PrepCommandMessage,
+  ):
+    | {
+        boardUnitCount?: number;
+        boardPlacements?: NonNullable<PrepCommandMessage["boardPlacements"]>;
+        xpPurchaseCount?: number;
+        shopRefreshCount?: number;
+        shopBuySlotIndex?: number;
+        shopLock?: boolean;
+        benchToBoardCell?: { benchIndex: number; cell: number };
+        benchSellIndex?: number;
+        boardSellIndex?: number;
+      }
+    | undefined {
+    if (
+      message.boardUnitCount === undefined &&
+      message.boardPlacements === undefined &&
+      message.xpPurchaseCount === undefined &&
+      message.shopRefreshCount === undefined &&
+      message.shopBuySlotIndex === undefined &&
+      message.shopLock === undefined &&
+      message.benchToBoardCell === undefined &&
+      message.benchSellIndex === undefined &&
+      message.boardSellIndex === undefined
+    ) {
+      return undefined;
+    }
+
+    return {
+      ...(message.boardUnitCount !== undefined && { boardUnitCount: message.boardUnitCount }),
+      ...(message.boardPlacements !== undefined && { boardPlacements: message.boardPlacements }),
+      ...(message.xpPurchaseCount !== undefined && { xpPurchaseCount: message.xpPurchaseCount }),
+      ...(message.shopRefreshCount !== undefined && { shopRefreshCount: message.shopRefreshCount }),
+      ...(message.shopBuySlotIndex !== undefined && { shopBuySlotIndex: message.shopBuySlotIndex }),
+      ...(message.shopLock !== undefined && { shopLock: message.shopLock }),
+      ...(message.benchToBoardCell !== undefined && { benchToBoardCell: message.benchToBoardCell }),
+      ...(message.benchSellIndex !== undefined && { benchSellIndex: message.benchSellIndex }),
+      ...(message.boardSellIndex !== undefined && { boardSellIndex: message.boardSellIndex }),
+    };
   }
 
   private handleAdminQuery(client: Client, message: AdminQueryMessage): void {
